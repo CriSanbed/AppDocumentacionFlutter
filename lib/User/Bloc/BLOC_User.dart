ade1eb61f4be955e85787cb8bf9697f9fecf255a
CriSanbed <cisanchez@itsqmet.edu.ec>
2021-07-25 01:22:34 -0500

    Agregando dependencias, arquitectura Bloc Pattern

---
+++ lib/User/Bloc/BLOC_User.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+// CREANDO LA CLASE
+class UserBloc implements Bloc{
+  @override
+  void dispose() {
+
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:app_documentar/User/Bloc/BLOC_User.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'Login',
+          home: const Login(),
+          initialRoute: 'main',
+          routes: {'main': (context) => const Login()},
+        ),
+        bloc: UserBloc());
+  }
+}
